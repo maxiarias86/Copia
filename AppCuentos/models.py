7fdd61bd07888b6381d4f0b2d67730ac3880f56b
maxiarias <maxi@MacBook-Air-de-Maxi.local>
2023-04-24 22:22:06 -0300

    Empece con la AppCuentos

--- AppCuentos/models.py
+++ AppCuentos/models.py
@@ -1,5 +1,16 @@
 from django.db import models
- 
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# ponerlas en el fomulario como choice categorias=['Fantasia','Micro-Relato','Ciencia Ficción','Policial','Fábula','Terror']
+
+class Cuento(models.Model):
+    fecha=models.DateField()
+    imagen=models.ImageField()
+    categoria=models.CharField(max_length=20)
+    titulo=models.CharField(max_length=50, unique=True)
+    subtitulo=models.CharField(max_length=100)
+    cuerpo=models.CharField(max_length=100000)
+    autor=models.ForeignKey(User, on_delete=models.CASCADE)
 
